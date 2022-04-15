fcec38f6bb7ddb9b2ad239a19809081526bff179
Axel <axelbaco@hotmail.com>
2022-04-15 20:41:04 -0300

    BaSP-W05-2022: ex01-variables and operators

--- semana-05/exercises/01-variables-and-operators.js
+++ semana-05/exercises/01-variables-and-operators.js
@@ -1,18 +1,26 @@
+console.log('--EXERCISE 1: VARIABLES AND OPERATORS');
+
 //A - Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+
+console.log('-Exercise 1.a: ');
 
 var num1 = 2, num2 = 4; 
 var result = num1+num2;
 
-console.log('A = ' + result);
+console.log(result);
 
 //B - Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
 
+console.log('-Exercise 1.b: ');
+
 var string1 = 'Radium', string2 = 'Rocket', result = (string1+string2);
 
-console.log('B = ' + result);
+console.log(result);
 
 //C - Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
 
+console.log('-Exercise 1.c: ');
+
 var string1 = 'Radium', string2 = 'Rocket', result = (string1.length + string2.length);
 
-console.log('C = ' + result);
+console.log(result);
